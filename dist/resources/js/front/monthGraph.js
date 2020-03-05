60a2c6ade1f8673e0971a8beeb7b21b93db78070
ldk0225 <ldk0225@midasit.com>
2020-03-05 20:30:44 +0900

    monthGraph 에니메이션 효과 완료

--- dist/resources/js/front/monthGraph.js
+++ dist/resources/js/front/monthGraph.js
@@ -1,6 +1,7 @@
     
     monthGraph = () => {
         const monthData=new Map();
+        let graph = {min : 123457890, max: 0}, dateList = [];
         
         const monthGraph = {
             init() {
@@ -9,34 +10,53 @@
                 drawDOM.innerHTML = this.draw(drawDOM);
             },
             draw(DOM) {
-                const t = '', len = monthData.size;
-                //width : 1060
-                //height: 350
-                for(let i=0; i<len; i++) {
-                    t+= '<div></div>'
-                }
+                let t = '', len = monthData.size, left=(1060/len)-5;
                 
+                for(let i=0; i<len; i++) {
+                    const dateCost = monthData.get(dateList[i]);
+                    const XbarHeight = 350*(dateCost) / graph.max;
+                    t+= `
+                    <div class="draw_Xbararea" style="margin-left:${i === 0 ? 0 : left}px">
+                        <div class="draw_dateCost" style="bottom:${XbarHeight}px">${dateCost}</div>
+                        <div class="draw_Xbar" style="height:${XbarHeight}px"></div>
+                        <div class="draw_dateText">${dateList[i]}</div>
+                    </div>
+                    `;
+                }
+                setTimeout(this.callbackAnimation, 100, DOM);
                 return t;
             },
             setData() {
             //그래프 그려줄 데이터 셋팅
             const recordDate ='이용일자';
             const recordCost ='이용금액';
+        
                 for(let i=0; i<EXCEL_JSON.length-1; i++) {
                     //key: date, value : cost
+                    const tmpCost = parseInt((EXCEL_JSON[i][recordCost]).replace(/,/g,''));
+                    if(!dateList.includes(EXCEL_JSON[i][recordDate])){ dateList.push(EXCEL_JSON[i][recordDate]); }
                     if(!monthData.has(EXCEL_JSON[i][recordDate]) && (EXCEL_JSON[i][recordDate].length)) {
-                        monthData.set(EXCEL_JSON[i][recordDate], Number((EXCEL_JSON[i][recordCost]).replace(/,/g,'')));
+                        monthData.set(EXCEL_JSON[i][recordDate], tmpCost);
+                        graph.min = Math.min(graph.min, tmpCost);
+                        graph.max = Math.max(graph.max, tmpCost);
                     }
                     //중복된 날자가 있다면 기존cost에서 더함 
                     else if(monthData.has(EXCEL_JSON[i][recordDate]) && (EXCEL_JSON[i][recordDate].length)) {
-                        const tmpCost = monthData.get(EXCEL_JSON[i][recordDate]);
-                        monthData.set(EXCEL_JSON[i][recordDate], parseInt((EXCEL_JSON[i]['이용금액']).replace(/,/g,'')) + tmpCost);
+                        const _Cost = monthData.get(EXCEL_JSON[i][recordDate]);
+                        monthData.set(EXCEL_JSON[i][recordDate], (tmpCost + _Cost));
+                        graph.min = Math.min(graph.min, (tmpCost + _Cost));
+                        graph.max = Math.max(graph.max, (tmpCost + _Cost));
                     }
                 }
                 console.log(monthData);
+            },
+            callbackAnimation(DOM) {
+                //init draw graph
+                DOM.classList.remove('draw_default');
+                
+                //
             }
         }
 
-        
         monthGraph.init();
     }
